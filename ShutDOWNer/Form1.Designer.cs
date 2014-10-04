7f0bf114ea385b3e936685460d330b3a10787588
Arcady Usov <itcrab@ya.ru>
2014-10-04 13:00:00 +0600

    Initial project.

---
+++ ShutDOWNer/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ShutDOWNer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.minutes = new System.Windows.Forms.ComboBox();
+            this.hours = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.variant = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(96, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "ВРЕМЯ";
+            // 
+            // minutes
+            // 
+            this.minutes.FormattingEnabled = true;
+            this.minutes.Location = new System.Drawing.Point(67, 33);
+            this.minutes.Name = "minutes";
+            this.minutes.Size = new System.Drawing.Size(58, 21);
+            this.minutes.TabIndex = 17;
+            // 
+            // hours
+            // 
+            this.hours.FormattingEnabled = true;
+            this.hours.Location = new System.Drawing.Point(67, 6);
+            this.hours.Name = "hours";
+            this.hours.Size = new System.Drawing.Size(58, 21);
+            this.hours.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Всего секунд:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Минуты:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Часы:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 50);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "ЗАПУСК";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(198, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Режим:";
+            // 
+            // variant
+            // 
+            this.variant.FormattingEnabled = true;
+            this.variant.Items.AddRange(new object[] {
+            "Выключить",
+            "Спящий режим"});
+            this.variant.Location = new System.Drawing.Point(67, 60);
+            this.variant.Name = "variant";
+            this.variant.Size = new System.Drawing.Size(146, 21);
+            this.variant.TabIndex = 21;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 133);
+            this.Controls.Add(this.variant);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.minutes);
+            this.Controls.Add(this.hours);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вырубалка 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox minutes;
+        private System.Windows.Forms.ComboBox hours;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox variant;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ShutDOWNer/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShutDOWNer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int hm;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                hours.Items.Add(i);
+            }
+
+            for (int i = 1; i <= 59; i++)
+            {
+                minutes.Items.Add(i);
+            }
+
+            hours.Text = "0";
+            minutes.Text = "1";
+            variant.Text = "Выключить";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int h, m;
+            h = Convert.ToInt32(hours.Text);
+            m = Convert.ToInt32(minutes.Text);
+            hm = (h*60+m)*60;
+
+            if (hm < 1) return;
+
+            label4.Text = hm.ToString();
+            progressBar1.Maximum = hm;
+            
+            if (timer1.Enabled != true)
+            {
+                button1.Text = "СТОП";
+                timer1.Enabled = true;
+            }
+            else
+            {
+                button1.Text = "ЗАПУСК";
+                timer1.Enabled = false;
+            }
+        }
+
+        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(hm>0) hm = hm - 1;
+            label4.Text = hm.ToString();
+            progressBar1.Value = hm;
+
+            if (hm == 0)
+            {
+                if (variant.Text == "Выключить")
+                {
+                    timer1.Enabled = false;
+                    System.Diagnostics.Process.Start("Shutdown", "-s -t 15");
+                }
+
+                if (variant.Text == "Спящий режим")
+                {
+                    timer1.Enabled = false;
+                    System.Diagnostics.Process.Start("rundll32.exe", "powrprof.dll,SetSuspendState");
+                }
+            }
+        }
+
+    }
+}
